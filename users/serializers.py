64615f7928b68007c963761f4f262f7936b36010
alexandr.lobanov <alexandp83@mail.ru>
2023-11-06 17:12:12 +0300

    UserSerializer model improved

--- users/serializers.py
+++ users/serializers.py
@@ -4,8 +4,10 @@ from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
     otp_code = serializers.CharField(required=False)
-
+    invite_code = serializers.CharField(required=False)
+    activated_user_phone_numbers = serializers.ListField(child=serializers.CharField(), read_only=True)
 
     class Meta:
         model = User
-        fields = ['phone_number', 'otp_code']
+        fields = ['phone_number', 'otp_code', 'invite_code', 'other_profile_invite_code',
+                  'activated_user_phone_numbers']
