e1a7cff3e6e2a68d9d77b4625ec332f49b2f073c
alexandr.lobanov <alexandp83@mail.ru>
2023-11-06 17:24:43 +0300

    UserProfileView improved

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 import random
+import string
 
 from rest_framework.generics import RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
@@ -22,12 +23,10 @@ class UserAuthenticationView(APIView):
 
             return Response({'otp_code': otp_code})
         else:
-            # invite_code = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits, k=6))
+            invite_code = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits, k=6))
             otp_code = random.randint(1000, 9999)
 
-            user = User.objects.create(phone_number=phone_number, otp_code=str(otp_code))
-
-            return Response({'otp_code': otp_code})
+            return Response({'otp_code': otp_code, 'invite_code': invite_code})
 
     def put(self, request):
         phone_number = request.data.get('phone_number')
@@ -49,9 +48,22 @@ class UserProfileView(RetrieveAPIView):
 
     def get_object(self):
         user = self.request.user
+        invite_code = user.invite_code
+
+        activated_user_phone_numbers = []
+
+        if invite_code:
+            activated_users = User.objects.filter(other_profile_invite_code=invite_code)
+            activated_user_phone_numbers = activated_users.values_list('phone_number', flat=True)
+
+        if not activated_user_phone_numbers:
+            activated_user_phone_numbers = ['No inviters']
 
         serialized_data = {
             'phone_number': user.phone_number,
+            'invite_code': invite_code,
+            'other_profile_invite_code': user.other_profile_invite_code,
+            'activated_user_phone_numbers': activated_user_phone_numbers,
         }
 
         return serialized_data
