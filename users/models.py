af28b4cf01f5721258c78765e89c96cad1a1b9f3
alexandr.lobanov <alexandp83@mail.ru>
2023-11-06 17:10:38 +0300

    user model improved

--- users/models.py
+++ users/models.py
@@ -9,6 +9,11 @@ class User(AbstractUser):
     phone_number = models.CharField(max_length=12, unique=True)
     otp_code = models.CharField(max_length=4)
 
+    invite_code = models.CharField(max_length=6, blank=True)
+    other_profile_invite_code = models.CharField(max_length=6, blank=True)
+
+    inviter = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='invited_users')
+
     def __str__(self):
         return self.phone_number
 
