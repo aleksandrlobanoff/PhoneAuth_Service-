da9b6e397b044ce576d234ab384c5a41536d6350
alexandr.lobanov <alexandp83@mail.ru>
2023-11-06 20:11:50 +0300

    pep8

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,6 @@ from users.views import UserAuthenticationView, UserProfileView
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-
 app_name = UsersConfig.name
 
 schema_view = get_schema_view(
