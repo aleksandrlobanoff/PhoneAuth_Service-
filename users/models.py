4d444d655328128996f9326fdeddae66ebe3b5ea
alexandr.lobanov <alexandp83@mail.ru>
2023-11-06 16:23:20 +0300

    a custom user model has been created

--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    email = None
+
+    phone_number = models.CharField(max_length=12, unique=True)
+    otp_code = models.CharField(max_length=4)
+
+    def __str__(self):
+        return self.phone_number
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = []
+
+    def check_otp_code(self, otp_code):
+        return self.otp_code == otp_code
+
+    def __str__(self):
+        return self.phone_number
