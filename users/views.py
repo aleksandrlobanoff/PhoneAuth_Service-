f5c30c2a7dda0009511c9953cf6d8f895a46563d
alexandr.lobanov <alexandp83@mail.ru>
2023-11-06 16:56:57 +0300

    added put method

--- users/views.py
+++ users/views.py
@@ -1,8 +1,8 @@
 import random
-import string
 
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from users.models import User
 
@@ -25,3 +25,16 @@ class UserAuthenticationView(APIView):
             user = User.objects.create(phone_number=phone_number, otp_code=str(otp_code))
 
             return Response({'otp_code': otp_code})
+
+    def put(self, request):
+        phone_number = request.data.get('phone_number')
+        otp_code = request.data.get('otp_code')
+
+        user = User.objects.filter(phone_number=phone_number).first()
+        if not user or not user.check_otp_code(otp_code):
+            return Response({'message': 'Invalid credentials'}, status=400)
+
+        refresh = RefreshToken.for_user(user)
+        access_token = str(refresh.access_token)
+
+        return Response({'access_token': access_token})
